5f298c9048c405dad24b8fe7d477d67faec04935
Adam Eskreis <adam@citymaps.com>
2014-11-12 19:49:26 -0500

    post to slack

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,16 +8,20 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
+  final val slackPostURL = "https://citymaps.slack.com/services/hooks/slackbot?token=Zl151cb5dnKoDR9VjjEq3TzN&channel=%s"
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def kitty = Action.async {
+  def kitty = Action.async { implicit request =>
     WS.url("http://thecatapi.com/api/images/get?format=src&type=gif")
       .withFollowRedirects(false)
       .get()
       .map { data =>
-      Ok(data.header("Location").getOrElse("")).as("text/plain")
+      val slackUrl = String.format(slackPostURL, request.getQueryString("channel_name").getOrElse(""))
+      WS.url(slackUrl).post(data.header("Location").getOrElse(""))
+      Ok("")
     }
   }
 
